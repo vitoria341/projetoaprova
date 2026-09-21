91c177e5c575e17c12b76d045804a3df43906dd2
vitoria341 <vitoria250908@gmail.com>
2026-09-21 08:22:36 -0400

    A

--- Configs/Banco de dados Aprova+.sql
+++ Configs/Banco de dados Aprova+.sql
@@ -8,7 +8,7 @@ email_usu VARCHAR(100),
 senha_usu VARCHAR(100)
 );
 
-CREATE TABLE materia (
+CREATE TABLE material (
 id_mat INT PRIMARY KEY AUTO_INCREMENT,
 nome_mat VARCHAR(100),
 descricao_mat VARCHAR(200)
---
+++ DAO/Material.cs
@@ -0,0 +1,9 @@
+﻿namespace projetogrupo.DAO
+{
+    public class Material
+    {
+        public int Id_mat { get; set; }
+        public string Nome_mat { get; set; } = string.Empty;
+        public string Descricao_mat { get; set; } = string.Empty;
+    }
+}
