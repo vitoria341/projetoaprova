8aa942e0a6246caf994a3aa35f6aacdc19289355
Isabella0541 <bellapachu@gmail.com>
2026-09-22 13:36:42 -0400

    arrumado

--- Models/Topico.cs
+++ Models/Topico.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Nome { get; set; } = string.Empty;
-        public string Id_mat_fk { get; set; } = string.Empty;
-        public string Id_mat { get; set; } = string.Empty;
+        
+        
     }
 }
